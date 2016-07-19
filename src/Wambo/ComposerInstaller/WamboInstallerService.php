732bcb3a4a5b0460df80e43e283bfd1f0125a6f2
Rene Penner <rene@penner.name>
2016-07-19 20:21:49 +0200

    reverse

--- src/Wambo/ComposerInstaller/WamboInstallerService.php
+++ src/Wambo/ComposerInstaller/WamboInstallerService.php
@@ -21,6 +21,28 @@ class WamboInstallerService
 
     /**
      * @return string
+     * @throws \Exception
+     */
+    public function getAutoloadNamespace()
+    {
+        $autoload = $this->package->getAutoload();
+
+        if ( !array_key_exists(WamboInstaller::AUTOLOAD_TYPE, $autoload) ) {
+            throw new \Exception('No PSR-4 Namespace found in package');
+        }
+
+        $namespaces = $autoload[WamboInstaller::AUTOLOAD_TYPE];
+        $namespace_array = array_keys($namespaces);
+
+        if (count($namespace_array) !== 1) {
+            throw new \Exception('No unique PSR-4 namespace.');
+        }
+
+        return $namespace_array[0];
+    }
+
+    /**
+     * @return string
      */
     public function getBootstrapClassName() : string
     {
