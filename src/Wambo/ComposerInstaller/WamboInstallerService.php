0e8db19c7e407d30fe6362ec5ff63ed77891edbb
Rene Penner <rene@penner.name>
2016-07-19 20:16:44 +0200

    fix foo

--- src/Wambo/ComposerInstaller/WamboInstallerService.php
+++ src/Wambo/ComposerInstaller/WamboInstallerService.php
@@ -3,7 +3,6 @@
 namespace Wambo\ComposerInstaller;
 
 use Composer\Package\PackageInterface;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Wambo\ComposerInstaller\Exception\InvalidArgumentException;
 
 class WamboInstallerService
@@ -18,28 +17,6 @@ class WamboInstallerService
     public function __construct(PackageInterface $package)
     {
         $this->package = $package;
-    }
-
-    /**
-     * @return string
-     * @throws \Exception
-     */
-    public function getAutoloadNamespace()
-    {
-        $autoload = $this->package->getAutoload();
-
-        if ( !array_key_exists(WamboInstaller::AUTOLOAD_TYPE, $autoload) ) {
-            throw new \Exception('No PSR-4 Namespace found in package');
-        }
-
-        $namespaces = $autoload[WamboInstaller::AUTOLOAD_TYPE];
-        $namespace_array = array_keys($namespaces);
-
-        if (count($namespace_array) !== 1) {
-            throw new \Exception('No unique PSR-4 namespace.');
-        }
-
-        return $namespace_array[0];
     }
 
     /**
